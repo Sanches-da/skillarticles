37ccf8fabcd314af161a3e446e796e2cdf82e649
Sanches_da <dudnik2104@gmail.com>
2019-12-29 21:36:03 +0200

    hometask_3

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/NestedScrollBehaviorBottom.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/NestedScrollBehaviorBottom.kt
@@ -6,6 +6,8 @@ import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import kotlin.math.max
+import kotlin.math.min
 
 class NestedScrollBehaviorBottom<T : ConstraintLayout> constructor(context: Context?, attrs: AttributeSet?) : CoordinatorLayout.Behavior<T>(context, attrs){
     private var height: Int = 0
@@ -28,6 +30,19 @@ class NestedScrollBehaviorBottom<T : ConstraintLayout> constructor(context: Cont
         type: Int
     ) = axes == ViewCompat.SCROLL_AXIS_VERTICAL
 
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: T,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+    }
+
     override fun onNestedScroll(
         coordinatorLayout: CoordinatorLayout,
         child: T,
@@ -38,13 +53,13 @@ class NestedScrollBehaviorBottom<T : ConstraintLayout> constructor(context: Cont
         dyUnconsumed: Int,
         type: Int
     ) {
-        child.apply {
-            //clearAnimation()
-            //animate()
-            //    .translationY(if (dyConsumed > 0) height.toFloat() else 0f)
-                //.duration = 200
-            //visibility = if (dyConsumed > 0) View.GONE else View.VISIBLE
-            translationY =if (dyConsumed > 0) height.toFloat() else 0f
-        }
+//        child.apply {
+//            //clearAnimation()
+//            //animate()
+//            //    .translationY(if (dyConsumed > 0) height.toFloat() else 0f)
+//                //.duration = 200
+//            //visibility = if (dyConsumed > 0) View.GONE else View.VISIBLE
+//            translationY =if (dyConsumed > 0) height.toFloat() else 0f
+//        }
     }
 }
